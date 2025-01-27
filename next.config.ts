e18871924c4627973001bc546453e465f4e09fa6
saima <saiamqadri008@gmail.com>
2025-01-27 23:00:56 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["unsplash.com"], // Added unsplash.com domain
+  },
 };
 
 export default nextConfig;
